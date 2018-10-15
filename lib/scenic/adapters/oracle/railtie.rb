a8cf371451457a3cd532b4a91c4c30ecd243f35e
Chris Dinger <chris@houseofding.com>
2018-10-15 12:05:51 -0500

    Add railtie for Scenic's SchemaDumper
    This has to be explicitly added to oracle-enhanced's SchemaDumper for it
    to work properly.

---
+++ lib/scenic/adapters/oracle/railtie.rb
@@ -0,0 +1,14 @@
+require "rails/railtie"
+require "active_record/connection_adapters/oracle_enhanced_adapter"
+
+module Scenic
+  module Adapters
+    class Oracle
+      class Railtie < Rails::Railtie
+        ActiveSupport.on_load(:active_record) do
+          ActiveRecord::ConnectionAdapters::OracleEnhanced::SchemaDumper.prepend Scenic::SchemaDumper
+        end
+      end
+    end
+  end
+end
--- lib/scenic/oracle_adapter.rb
+++ lib/scenic/oracle_adapter.rb
@@ -4,6 +4,7 @@ require "active_record"
 require "scenic"
 require "scenic/oracle_adapter/version"
 require "scenic/adapters/oracle"
+require "scenic/adapters/oracle/railtie" if defined?(Rails)
 
 module Scenic
   module OracleAdapter
