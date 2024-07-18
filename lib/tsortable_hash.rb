8a0c40d089a26ed1c54a4676fde9ef8bdad0ab0d
Chris Dinger <cdinger@gmail.com>
2024-07-18 23:19:24 +0700

    Don't use anonymous block syntax
    Anonymous blocks were introduced in 3.1. This change the block syntax to
    accommodate pre-3.1 rubies. 3.0 and older are technically no longer supported,
    but this is a simple change that opens up compatibility.

--- lib/tsortable_hash.rb
+++ lib/tsortable_hash.rb
@@ -4,7 +4,7 @@ class TSortableHash < Hash
   include TSort
 
   alias_method :tsort_each_node, :each_key
-  def tsort_each_child(node, &)
-    fetch(node).each(&)
+  def tsort_each_child(node, &block)
+    fetch(node).each(&block)
   end
 end
