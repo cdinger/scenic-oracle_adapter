6631aa2f228ec2251264d1b2d57859875f220456
Chris Dinger <cdinger@gmail.com>
2025-03-07 07:45:54 -0600

    Require tsort before scenic
    The Scenic::Adapters::Postgres::Views::TSortableHash class is including
    TSort, but doesn't have a require statement. When we `require "scenic"`,
    it fails with:
    
    ```
    uninitialized constant
    Scenic::Adapters::Postgres::Views::TSortableHash::TSort
    ```
    
    Adding this before scenic ensure TSort is available. We should open a PR
    to scenic to add this there.

--- lib/scenic/oracle_adapter.rb
+++ lib/scenic/oracle_adapter.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require "active_record"
+# Require tsort here before scenic to avoid:
+# uninitialized constant Scenic::Adapters::Postgres::Views::TSortableHash::TSort
+require "tsort"
 require "scenic"
 require "scenic/oracle_adapter/version"
 require "scenic/adapters/oracle"
